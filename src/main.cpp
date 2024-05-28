8298d6b336e51572f683633938f9a10e27d7b548
Adesola Samuel <adesolasamuel2018@gmail.com>
2024-05-28 10:16:58 +0100

    All initial functionality setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,196 @@
+/*********************************************************************
+ *  ROSArduinoBridge
+ 
+
+    Created for the Pi Robot Project: http://www.pirobot.org
+    and the Home Brew Robotics Club (HBRC): http://hbrobotics.org
+    
+    Authors: Patrick Goebel, James Nugen
+
+    Inspired and modeled after the ArbotiX driver by Michael Ferguson
+    
+    Software License Agreement (BSD License)
+
+    Copyright (c) 2012, Patrick Goebel.
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above
+       copyright notice, this list of conditions and the following
+       disclaimer in the documentation and/or other materials provided
+       with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/* Serial port baud rate */
+#define BAUDRATE     57600
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+/* Maximum PWM signal */
+#define MAX_PWM        255
+
+/* Include definition of serial commands */
+#include "commands.h"
+
+/* Sensor functions */
+#include "sensors.h"
+
+/* Motor driver function definitions */
+#include "motor_driver.h"
+
+#define AUTO_STOP_INTERVAL 2000
+long lastMotorCommand = AUTO_STOP_INTERVAL;
+
+
+// A pair of varibles to help parse serial commands (thanks Fergs)
+int arg = 0;
+int index = 0;
+
+// Variable to hold an input character
+char chr;
+
+// Variable to hold the current single-character command
+char cmd;
+
+// Character arrays to hold the first and second arguments
+char argv1[16];
+char argv2[16];
+
+// The arguments converted to integers
+long arg1;
+long arg2;
+
+/* Clear the current command parameters */
+void resetCommand() {
+  cmd = NULL;
+  memset(argv1, 0, sizeof(argv1));
+  memset(argv2, 0, sizeof(argv2));
+  arg1 = 0;
+  arg2 = 0;
+  arg = 0;
+  index = 0;
 }
+
+/* Run a command.  Commands are defined in commands.h */
+int runCommand() {
+  int i = 0;
+  char *p = argv1;
+  char *str;
+  int pid_args[4];
+  arg1 = atoi(argv1);
+  arg2 = atoi(argv2);
+  
+  switch(cmd) {
+  case GET_BAUDRATE:
+    Serial.println(BAUDRATE);
+    break;
+  case ANALOG_READ:
+    Serial.println(analogRead(arg1));
+    break;
+  case DIGITAL_READ:
+    Serial.println(digitalRead(arg1));
+    break;
+  case ANALOG_WRITE:
+    analogWrite(arg1, arg2);
+    Serial.println("OK"); 
+    break;
+  case DIGITAL_WRITE:
+    if (arg2 == 0) digitalWrite(arg1, LOW);
+    else if (arg2 == 1) digitalWrite(arg1, HIGH);
+    Serial.println("OK"); 
+    break;
+  case PIN_MODE:
+    if (arg2 == 0) pinMode(arg1, INPUT);
+    else if (arg2 == 1) pinMode(arg1, OUTPUT);
+    Serial.println("OK");
+    break;
+  case PING:
+    Serial.println(Ping(arg1));
+    break;
+  case MOTOR_SPEEDS:
+    /* Reset the auto stop timer */
+    lastMotorCommand = millis();
+    if (arg1 == 0 && arg2 == 0) {
+      setMotorSpeeds(0, 0);
+    }
+    break;
+  case MOTOR_RAW_PWM:
+    /* Reset the auto stop timer */
+    lastMotorCommand = millis();
+    setMotorSpeeds(arg1, arg2);
+    Serial.println("OK"); 
+    break;
+  default:
+    Serial.println("Invalid Command");
+    break;
+  }
+}
+
+/* Setup function--runs once at startup. */
+void setup() {
+  Serial.begin(BAUDRATE);
+  initMotorController();
+
+}
+
+/* Enter the main loop.*/
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  while (Serial.available() > 0) {
+    
+    // Read the next character
+    chr = Serial.read();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Terminate a command with a CR
+    if (chr == 13) {
+      if (arg == 1) argv1[index] = NULL;
+      else if (arg == 2) argv2[index] = NULL;
+      runCommand();
+      resetCommand();
+    }
+    // Use spaces to delimit parts of the command
+    else if (chr == ' ') {
+      // Step through the arguments
+      if (arg == 0) arg = 1;
+      else if (arg == 1)  {
+        argv1[index] = NULL;
+        arg = 2;
+        index = 0;
+      }
+      continue;
+    }
+    else {
+      if (arg == 0) {
+        // The first arg is the single-letter command
+        cmd = chr;
+      }
+      else if (arg == 1) {
+        // Subsequent arguments can be more than one character
+        argv1[index] = chr;
+        index++;
+      }
+      else if (arg == 2) {
+        argv2[index] = chr;
+        index++;
+      }
+    }
+  }
+  
 }
